6ebb26320a1697bc0e2be47f3c7e98b72144b414
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2017-03-06 15:26:15 -0500

    groupon scraper works

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,21 +1,18 @@
+# frozen_string_literal: true
 class PreferencesController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
 
+  def new
+    # TODO: get preferences from user (location and massage preferences)
+    url = "https://www.groupon.com/browse/new-york?lat=40.7411955&lng=-73.99591570000001&address=Near+Me&query=massage&locale=en_US&sort=distance"
+    GrouponScraper.save_nearby_massages(url, 5, current_user)
+  end
 
-	def new
-	end
+  def create; end
 
-	def create
-	end
+  private
 
-	private
-
-	def user_params
-		params.require(:user).permit( 
-			massage_attributes: [:id, :address, :title, :rating, :rating_count,
-				:fine_print, :merchant_profile]
-		)
-	end
+  def user_params
+    params.require(:user).permit
+  end
 end
-
-
--- app/services/groupon_scraper.rb
+++ app/services/groupon_scraper.rb
@@ -1,5 +1,93 @@
+# frozen_string_literal: true
 class GrouponScraper
-	def self.get_nearby_massages
-	end
-	
+  def self.save_nearby_massages(url, distance, current_user)
+    doc = Nokogiri::HTML(open(url))
+    links = get_nearby_massage_links(doc, distance)
+    links.each do |link|
+      massage = create_massage_from_link(link)
+      current_user.massages << massage
+    end
+  end
+
+  private_class_method
+
+  def self.get_nearby_massage_links(doc, distance)
+    # offset of 1 to reject duplicate featured deal url
+    doc.css(".card-ui a")[1..-1].each_with_object([]) do |card, links|
+      links << card["href"] if within_distance?(card, distance)
+    end
+  end
+
+  def self.within_distance?(card, distance)
+    dist_str = card.ancestors("#pull-cards").at_css(".cui-location-distance").text
+    format_dist_str(dist_str) <= distance.to_f
+  end
+
+  def self.format_dist_str(str)
+    str.strip[/\d.\d/, 0].to_f
+  end
+
+  def self.create_massage_from_link(link)
+    doc = Nokogiri::HTML(open(link))
+    attrs = massage_attrs(doc, link)
+    Massage.create(attrs)
+  end
+
+  def self.massage_attrs(doc, link)
+    {
+      address: address(link),
+      title: title(doc),
+      rating: rating(doc),
+      rating_count: rating_count(doc),
+      fine_print: fine_print(doc)
+      # merchant_profile: merchant_profile(doc)
+    }
+  end
+
+  def self.address(link)
+    @address ||= find_address(link)
+  end
+
+  def self.find_address(link)
+    driver = Selenium::WebDriver.for :chrome
+    driver.get link
+    wait = Selenium::WebDriver::Wait.new(timeout: 10)
+    # TODDO class incentive always on page -> think of different condition
+    if driver.find_element(class: "incentive")
+      driver.find_element(id: "nothx").click
+      # @address = driver.find_element(class: "address").text
+    end
+    wait.until { driver.find_element(class: "address-content") }
+    address = driver.find_element(class: "address-content").text
+    driver.quit
+    address
+  end
+
+  def self.title(doc)
+    doc.at_css("#deal-title").text.strip
+  end
+
+  def self.rating(doc)
+    return unless doc.css(".product-reviews-rating li")
+    star_count = 0
+    doc.css(".product-reviews-rating li").each do |star|
+      star_count += 1 if star.css(".full-star").present?
+      star_count += 0.5 if star.css(".half-star").present?
+    end
+    star_count
+  end
+
+  def self.rating_count(doc)
+    return 0 unless doc.at_css(".star-rating-text")
+    doc.at_css(".star-rating-text").text.gsub(/\D+/, "")
+  end
+
+  def self.fine_print(doc)
+    doc.at_css(".fine-print span").text + " " + doc.at_css(".fine-print span:nth-of-type(2)").text
+  end
+
+  # def self.merchant_profile(doc)
+  # 	#TODO find proper selectors for this
+  # 	doc.at_css(".merchant-profile p:nth-of-type(2)").text
+  # end
 end
